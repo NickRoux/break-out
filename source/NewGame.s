921389c95be56eb627ce410518e3fd0743fa1a4d
Nik <nik@nik-M68MT-S2.(none)>
2014-02-21 02:07:53 -0700

    essentially unchanged. removed personal information

--- source/NewGame.s
+++ source/NewGame.s
@@ -4,6 +4,7 @@
 NewGame:
 	push	{r0-r11,lr}
 
+// notice absence of r6/r7. 
 		ycoor	.req	r0
 		xcoor	.req	r1
 		height	.req	r2
@@ -18,39 +19,39 @@ background:
 
 		ldr 	ycoor, =0
 		ldr 	xcoor, =0
-		ldr 	height, =767					//must use 767 X 1023 since we're starting at 0,0
+		ldr 	height, =767					//must use 767 X 1023 since we're starting at 0,0 instead of 1,1
 		ldr 	width, =1023
-		ldr 	colour, =0xf3f2					// nice greenish colour
+		ldr 	colour, =0xf3f2					// deep background colour. please don't change as it took long time to figure this one out 
 		ldr	addr, [r7, #32]
 		bl	drawShape
 
 		/*border*/
 		ldr 	ycoor, =10
 		ldr 	xcoor, =10
-		ldr 	height, =747					//must use 767 X 1023 since we're starting at 0,0
+		ldr 	height, =747					
 		ldr 	width, =1003
 		ldr 	colour, =0xff11					// nice greenish colour
 		ldr	addr, [r7, #32]
 		bl	drawShape
 
-		/*invisible wall on the right*/
+		/*invisible wall on the right. Achived through implementing th coulour that is 'one-ff' from backgorund colour*/
 		ldr 	ycoor, =10
 		ldr 	xcoor, =843
-		ldr 	height, =747					//must use 767 X 1023 since we're starting at 0,0
+		ldr 	height, =747					
 		ldr 	width, =10
-		ldr 	colour, =0xf3f2					// ALMOST hte colour of the BG
+		ldr 	colour, =0xf3f2					// ALMOST the colour of the BG
 		ldr	addr, [r7, #32]
-		// ldr 	colour, =0x0000					// black
+		// ldr 	colour, =0x0000					// black colour, in case we decide to go with black border instead of invisible one
 
 		bl	drawShape
 
-		/*Heart*/
+		/* drwaw heart shape. Heart's position is rigid. to change refer to heart's documentation*/
 		bl 	drawHeart
 		/*Blocks*/
 		bl 	drawBlocks
 		/*peddle*/
 		bl 	drawPeddle
-		/*Three Balls one for every life*/
+		/*Three Balls, one for every life. One gets removed every time player looses life.*/
 		ldr 	ycoor,	=540
 		ldr		xcoor,	=930
 		ldr 	height, =5					
--- source/main.s
+++ source/main.s
@@ -29,30 +29,31 @@ main:
 
 //------------------------------
 
+//Initialize SNES connection as well as frame buffer for feuter use 
 
 	bl	SNESinit
 	bl	InitFrameBuffer
+// r6 is a globabl value holdiong return value of the SNESinit initialization sequence
+// r7 is the global value holding return value of the InitFrameBuffer sequence
+// note, r7 is heavily used. avoid using r7 without saving register window
 	ldr	r6, [r0,#32]
 	mov	r7, r0
 
+//starting the game sequence
 restart:
+
+//start of the game and initialization of main game logic functionalities
 	bl	NewGame				
 	
+//creation of the menue screen and initialization of the logic required to run the menue screen
 	bl	menuScreen
 	
-
+//r12 hold return value associated with the choice made in the menue screen
+// upon comparin, determines to either start over or to proceed to end sequence
 	cmp	r12, #1
 	bne	restart
 	
 
-
-//-------------------------------------------------------------------------------------------------
-
-	mov	r2, #1			// debugging
-
-
-// ------------------------------------------------------------------------
-	
 
 haltLoop$:
 	b		haltLoop$
